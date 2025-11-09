e8e62a47840b8e0aa0ffbe38137a63f26eacea7c
Ghama <ghamaalfajri@gmail.com>
2025-11-09 22:29:21 +0700

    Enable SSL for database connection
    Added SSL configuration to the database connection pool.

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,13 +2,17 @@ const { Pool } = require('pg'); // [cite: 559]
 require('dotenv').config(); // [cite: 560]
 
 const pool = new Pool({
-  user: process.env.DB_USER, // [cite: 562]
-  host: process.env.DB_HOST, // [cite: 563]
-  database: process.env.DB_DATABASE, // [cite: 564]
-  password: process.env.DB_PASSWORD, // [cite: 565]
-  port: process.env.DB_PORT, // [cite: 566]
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: {
+    require: true,
+  }
 });
 
 module.exports = {
   query: (text, params) => pool.query(text, params), // [cite: 569]
+
 };
